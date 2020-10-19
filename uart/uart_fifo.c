46a541be482fcf5cf51799e1f7aeda4a33ebb6dd
danimolsen1988 <201808188@post.au.dk>
2020-10-19 19:07:56 +0200

    Update uart_fifo.c

--- uart/uart_fifo.c
+++ uart/uart_fifo.c
@@ -11,15 +11,10 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "app_uart.h"
-//#include "app_error.h"
-//#include "nrf_delay.h"
-//#include "nrf.h"
 #include "nrf_uart.h"
-//#include "bsp.h"
 #include "boards.h"
 #include "app_fifo.h"
 #include "nrf_drv_uart.h"
-//#include "nrf_assert.h"
 #include "uart_fifo.h"
 
 // UART pins
@@ -34,46 +29,31 @@
 app_uart_buffers_t buffers;
 //static uint8_t rx_buf[UART_RX_BUF_SIZE];                                                    
 static uint8_t txBuf[UART_TX_BUF_SIZE];
-//shiit
+//current handles char
 static uint8_t tx_buffer[1];
-static uint8_t rx_buffer[1];
 
-/**< RX FIFO buffer for storing data received on the UART until the application fetches them using app_uart_get(). */
-//static app_fifo_t m_rx_fifo; 
+
 /**< TX FIFO buffer for storing data to be transmitted on the UART when TXD is ready. Data is put to the buffer on using app_uart_put(). */                             
 static app_fifo_t txFifo;  
 
-static nrf_drv_uart_t app_uart_inst = NRF_DRV_UART_INSTANCE(APP_UART_DRIVER_INSTANCE);
+//uart instance
+static nrf_drv_uart_t uartInstance = NRF_DRV_UART_INSTANCE(APP_UART_DRIVER_INSTANCE);
                             
 
 //Eventhandler
 static void uartEventhandler(nrf_drv_uart_event_t * p_event, void* p_context);
       
 
-uint32_t uartInit(nrf_uart_baudrate_t baud, bool parity){
-  //buffers.rx_buf = rxBuf;                                                              
-  //buffers.rx_buf_size = sizeof (rxBuf);                                                     
+uint32_t uartInit(nrf_uart_baudrate_t baud, bool parity){                                  
   buffers.tx_buf = txBuf;                                                              
-  buffers.tx_buf_size = sizeof (txBuf);                                                     
-  //uint32_t errCode = app_uart_init(P_COMM_PARAMS, &buffers, EVT_HANDLER, APP_IRQ_PRIORITY_LOWEST);
-  
+  buffers.tx_buf_size = sizeof (txBuf);  
   uint32_t errCode;
-
-/*
-  if (buffers == NULL) {
-        return NRF_ERROR_INVALID_PARAM;
-  }
-*/
-    // Configure buffer RX buffer.
-    //err_code = app_fifo_init(&m_rx_fifo, p_buffers->rx_buf, p_buffers->rx_buf_size);
-    //VERIFY_SUCCESS(err_code);
 
     // Configure buffer TX buffer.
     errCode = app_fifo_init(&txFifo, buffers.tx_buf, buffers.tx_buf_size);
     if(errCode != NRF_SUCCESS){
       return errCode;
-    }
-    
+    }    
 
     nrf_drv_uart_config_t config = NRF_DRV_UART_DEFAULT_CONFIG;
     config.baudrate = (nrf_uart_baudrate_t)NRF_UART_BAUDRATE_115200;
@@ -85,7 +65,7 @@ uint32_t uartInit(nrf_uart_baudrate_t baud, bool parity){
     config.pselrxd = RX_PIN;
     config.pseltxd = TX_PIN;
 
-    errCode = nrf_drv_uart_init(&app_uart_inst, &config, uartEventhandler);
+    errCode = nrf_drv_uart_init(&uartInstance, &config, uartEventhandler);
     if(errCode != NRF_SUCCESS){
       return errCode;
     }
@@ -94,7 +74,7 @@ uint32_t uartInit(nrf_uart_baudrate_t baud, bool parity){
 }
 
 uint32_t uartDeinit(){
-    nrf_drv_uart_uninit(&app_uart_inst);
+    nrf_drv_uart_uninit(&uartInstance);
     return NRF_SUCCESS;
 }
 
@@ -115,13 +95,13 @@ uint32_t uartTransmit(char * data, uint32_t length){
         // (in 'uartEventhandler') when all preceding bytes are transmitted.
         // But if UART is not transmitting anything at the moment, we must start
         // a new transmission here.
-        if (!nrf_drv_uart_tx_in_progress(&app_uart_inst)) {
+        if (!nrf_drv_uart_tx_in_progress(&uartInstance)) {
             // This operation should be almost always successful, since we've
             // just added a byte to FIFO, but if some bigger delay occurred
             // (some heavy interrupt handler routine has been executed) since
             // that time, FIFO might be empty already.
             if (app_fifo_get(&txFifo, tx_buffer) == NRF_SUCCESS) {
-                errCode = nrf_drv_uart_tx(&app_uart_inst, tx_buffer, 1);
+                errCode = nrf_drv_uart_tx(&uartInstance, tx_buffer, 1);
             }
         }
       }else {
@@ -131,21 +111,19 @@ uint32_t uartTransmit(char * data, uint32_t length){
     return errCode;
 }
 
-static void uartEventhandler(nrf_drv_uart_event_t * p_event, void* p_context) {
-    app_uart_evt_t app_uart_event;
+static void uartEventhandler(nrf_drv_uart_event_t * p_event, void* p_context) {    
     uint32_t errCode;
-
     switch (p_event->type) {
         case NRF_DRV_UART_EVT_RX_DONE:
             //no RX
             break;
         case NRF_DRV_UART_EVT_ERROR:            
-            (void)nrf_drv_uart_rx(&app_uart_inst, rx_buffer, 1);
+            //
             break;
         case NRF_DRV_UART_EVT_TX_DONE:
             // Get next byte from FIFO.
             if (app_fifo_get(&txFifo, tx_buffer) == NRF_SUCCESS) {
-                (void)nrf_drv_uart_tx(&app_uart_inst, tx_buffer, 1);
+                (void)nrf_drv_uart_tx(&uartInstance, tx_buffer, 1);
             }
             else {
                 // Last byte from FIFO transmitted, notify the application.
