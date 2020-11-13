6c0bced637903b30ba36b92373cdf55ea9653b9d
spejderlasse <54673837+spejderlasse@users.noreply.github.com>
2020-11-13 12:19:34 +0100

    tilføjet 1 byte til payload
    fra 8 til 9 bytes

--- code/ss_twr_resp/ss_resp_main.c
+++ code/ss_twr_resp/ss_resp_main.c
@@ -30,16 +30,16 @@
 #define RNG_DELAY_MS 40
 
 /* Frames used in the ranging process. See NOTE 2,3 below. */
-static uint8 rx_poll_msg[] = {0x88, 0x37, 0, 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-static uint8 tx_resp_msg[] = {0x88, 0x37, 0, 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static uint8 rx_poll_msg[] = {0x88, 0x37, 0, 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0 ,0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static uint8 tx_resp_msg[] = {0x88, 0x37, 0, 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0x00 , 0 ,0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 /* Length of the common part of the message (up to and including the function code, see NOTE 3 below). */
 #define ALL_MSG_COMMON_LEN 2
 
 /* Index to access some of the fields in the frames involved in the process. */
 #define ALL_MSG_SN_IDX 2
-#define RESP_MSG_POLL_RX_TS_IDX 19
-#define RESP_MSG_RESP_TX_TS_IDX 23
+#define RESP_MSG_POLL_RX_TS_IDX 20
+#define RESP_MSG_RESP_TX_TS_IDX 24
 #define RESP_MSG_TS_LEN 4	
 #define RESP_MSG_SOURCE_ID_IDX 11
 #define RESP_MSG_TARGET_ID_IDX 3
@@ -50,7 +50,7 @@ static uint8 frame_seq_nb = 0;
 
 /* Buffer to store received response message.
 * Its size is adjusted to longest frame that this example code is supposed to handle. */
-#define RX_BUF_LEN 29
+#define RX_BUF_LEN 30
 static uint8 rx_buffer[RX_BUF_LEN];
 
 /* Hold copy of status register state here for reference so that it can be examined at a debug breakpoint. */
